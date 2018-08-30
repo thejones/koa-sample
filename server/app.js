9f4744a5a7cb2c5143c90f2f1f5abca75b41c779
Alex Jones <alexander.t.jones@gmail.com>
2018-08-30 05:40:51 -0800

    Seperate app from index for better testing

---
+++ server/app.js
@@ -0,0 +1,40 @@
+const Koa = require("koa");
+const Router = require("koa-router");
+const bodyParser = require("koa-bodyparser");
+const { executionTime } = require("./middleware/executionTime");
+const { addCount } = require("./middleware/requestCounter");
+// Routers
+const messagesRouter = require("./messages/messages.router");
+const statsRouter = require("./stats/stats.router");
+// This one is not for the example - just lets me see something on the main index...
+const router = new Router();
+
+const app = new Koa();
+const PORT = process.env.PORT || 8081;
+
+// Checking how Koa wants to handle errors.
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.status = 400;
+    ctx.body = err.message;
+    console.log("Error handler:", err.message);
+  }
+});
+
+app.use(executionTime);
+app.use(bodyParser());
+app.use(addCount);
+
+router.get("/", async ctx => {
+  ctx.body = "Sample Koa application";
+});
+
+app.use(router.routes());
+app.use(messagesRouter.routes());
+app.use(statsRouter.routes());
+
+// Not 100% on Koa error handling.
+
+module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,39 +1,5 @@
-const Koa = require("koa");
-const Router = require("koa-router");
-const bodyParser = require("koa-bodyparser");
-const { executionTime } = require("./middleware/executionTime");
-// const { slowDown } = require("./middleware/slowDown");
-const { addCount } = require("./middleware/requestCounter");
-// Routers
-const messagesRouter = require("./messages/messages.router");
-const statsRouter = require("./stats/stats.router");
-// This one is not for the example - just lets me see something on the main index...
-const router = new Router();
+const app = require("./app");
 
-const app = new Koa();
-const PORT = process.env.PORT || 8081;
-
-app.use(executionTime);
-app.use(bodyParser());
-// Just to help test logging response time.
-// app.use(slowDown);
-app.use(addCount);
-
-router.get("/", async ctx => {
-  ctx.body = "Sample Koa application";
-});
-
-app.use(router.routes());
-app.use(messagesRouter.routes());
-app.use(statsRouter.routes());
-
-// Not 100% on Koa error handling.
-const server = app
-  .listen(PORT, () => {
-    console.log(`Koa server running on ${PORT} ✔`);
-  })
-  .on("error", err => {
-    console.error(err);
-  });
-
-module.exports = server;
+const port = process.env.PORT || 8081;
+const server = app.listen(port);
+console.info(`Listening to http://localhost:${port} 🚀`);
